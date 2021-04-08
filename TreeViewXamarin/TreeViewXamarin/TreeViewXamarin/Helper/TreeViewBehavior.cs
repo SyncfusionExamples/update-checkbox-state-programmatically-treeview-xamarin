e54df498d7ebb2cf534b21bd8f60fe1f2857ed00
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-04-08 13:10:47 +0530

    XAMARIN-37304 - Sample and screenshot added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/TreeViewBehavior.cs
@@ -0,0 +1,80 @@
+﻿using Syncfusion.TreeView.Engine;
+using Syncfusion.XForms.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class TreeViewBehavior : Behavior<SfTreeView>
+    {
+        #region Fields
+
+        SfTreeView TreeView;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(SfTreeView bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            TreeView = bindable;
+            TreeView.Loaded += TreeView_Loaded;
+        }
+
+        protected override void OnDetachingFrom(SfTreeView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            TreeView.Loaded -= TreeView_Loaded;
+            TreeView = null;
+        }
+        #endregion
+
+        #region Methods
+
+        private void TreeView_Loaded(object sender, TreeViewLoadedEventArgs e)
+        {
+            UpdateCheckBoxState();
+            TreeView.CheckBoxMode = CheckBoxMode.Recursive;
+        }
+
+        private void UpdateCheckBoxState()
+        {
+            var checkedNodes = TreeView.GetCheckedNodes();
+            foreach (var node in checkedNodes)
+            {
+                //Update child nodes based on parent's checked state.
+                if (node.ParentNode == null && node.HasChildNodes)
+                {
+                    setChildChecked(node);
+                }
+
+                //Update parent node based on child's checked state.
+                else if (node.ParentNode != null)
+                {
+                    if (node.ParentNode.ChildNodes.All(x => x.IsChecked == true))
+                    {
+                        node.ParentNode.IsChecked = true;
+                    }
+                    else node.ParentNode.IsChecked = null;
+                }
+
+            }
+        }
+
+        private void setChildChecked(TreeViewNode node)
+        {
+            foreach (var child in node.ChildNodes)
+            {
+                child.IsChecked = true;
+                if (child.HasChildNodes)
+                {
+                    setChildChecked(child);
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/ViewModel/FileManagerViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace TreeViewXamarin
+{
+    [Preserve(AllMembers = true)]
+    public class FileManagerViewModel
+    {
+        #region Fields
+        private ObservableCollection<object> checkedItems;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<object> CheckedItems
+        {
+            get { return checkedItems; }
+            set { this.checkedItems = value; }
+        }
+
+        public ObservableCollection<Folder> Folders { get; set; }
+        #endregion
+
+        #region Constructor
+        public FileManagerViewModel()
+        {
+            this.Folders = GetFiles();
+        }
+        #endregion
+
+        #region Method
+        private ObservableCollection<Folder> GetFiles()
+        {
+            var nodeImageInfo = new ObservableCollection<Folder>();
+
+            var doc = new Folder() { FileName = "Documents"};
+            var download = new Folder() { FileName = "Downloads"};
+            var mp3 = new Folder() { FileName = "Music"};
+            var pictures = new Folder() { FileName = "Pictures"};
+            var video = new Folder() { FileName = "Videos"};
+
+            var pollution = new File() { FileName = "Environmental Pollution.docx"};
+            var globalWarming = new File() { FileName = "Global Warming.ppt"};            
+
+            var games = new File() { FileName = "Game.exe"};
+            var tutorials = new File() { FileName = "Tutorials.zip"};
+            var typeScript = new File() { FileName = "TypeScript.7z"};
+            var uiGuide = new File() { FileName = "UI-Guide.pdf"};
+
+            var song = new File() { FileName = "Gouttes"};
+
+            var camera = new File() { FileName = "Camera Roll"};
+            var stone = new File() { FileName = "Stone.jpg"};
+            var wind = new File() { FileName = "Wind.jpg"};
+
+            var img0 = new SubFile() { FileName = "WIN_20160726_094117.JPG"};
+            var img1 = new SubFile() { FileName = "WIN_20160726_094118.JPG"};
+
+            var video1 = new File() { FileName = "Naturals.mp4"};
+            var video2 = new File() { FileName = "Wild.mpeg"};
+
+            doc.Files = new ObservableCollection<File>
+            {
+                pollution,
+                globalWarming
+            };
+
+            download.Files = new ObservableCollection<File>
+            {
+                games,
+                tutorials,
+                typeScript,
+                uiGuide
+            };
+
+            mp3.Files = new ObservableCollection<File>
+            {
+                song
+            };
+
+            pictures.Files = new ObservableCollection<File>
+            {
+                camera,
+                stone,
+                wind
+            };
+            camera.SubFiles = new ObservableCollection<SubFile>
+            {
+                img0,
+                img1
+            };
+
+            video.Files = new ObservableCollection<File>
+            {
+                video1,
+                video2
+            };
+
+            nodeImageInfo.Add(doc);
+            nodeImageInfo.Add(download);
+            nodeImageInfo.Add(mp3);
+            nodeImageInfo.Add(pictures);
+            nodeImageInfo.Add(video);
+
+            checkedItems = new ObservableCollection<object>();
+            checkedItems.Add(doc);
+            checkedItems.Add(typeScript);
+            checkedItems.Add(uiGuide);
+            checkedItems.Add(stone);
+            checkedItems.Add(pictures);
+            checkedItems.Add(song);
+
+            return nodeImageInfo;
+        }
+        #endregion
+    }
+}
